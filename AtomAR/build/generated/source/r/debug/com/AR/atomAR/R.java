0e0f734ed4f77e2b70e4081e12d3c0622175a9ab
AlexOuyang <ouyangchenxing@hotmail.com>
2016-05-29 04:30:39 -0700

    added shadow mapping rendering options

--- AtomAR/build/generated/source/r/debug/com/AR/atomAR/R.java
+++ AtomAR/build/generated/source/r/debug/com/AR/atomAR/R.java
@@ -33,7 +33,6 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int activity_shadows=0x7f030001;
     }
     public static final class menu {
         public static final int opengl_shadow_menu=0x7f080000;
